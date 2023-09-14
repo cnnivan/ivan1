9c711d0082b877a036c210974f73c0c127373df8
Ivan Cortez <ivan.cortez143@gmail.com>
2023-09-14 03:31:36 -0300

    version final
    agregando tercera pantalla y cambio final

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void Siguiente2(View v){
+        Intent sig2 = new Intent(this,MainActivity3.class);
+        startActivity(sig2);
     }
 }
